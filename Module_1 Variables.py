ee5631bb079638430631668eb9655bf4e74bc828
SophiISh <frolowasof@yandex.ru>
2024-08-25 15:21:16 +0300

    25/08/2024

---
+++ Module_1 Variables.py
@@ -0,0 +1,9 @@
+number_of_completed_homework_assignments=a=(12)
+number_of_hours_spent=b=(1.5)
+course_name=c='Python'
+time_for_one_task=d=(b/a)
+print(d)
+print("Курс:",c,",","всего задач:",a,",","затрачено часов:",b,",","среднее время выполнения:",d,"часа.")
+
+
+
