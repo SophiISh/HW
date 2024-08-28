f279e9e235377ffb88544e645f1d0a22b770a326
SophiISh <frolowasof@yandex.ru>
2024-08-28 19:21:32 +0300

    28/08/2024

---
+++ module_1_3_dynamic typing.py
@@ -0,0 +1,8 @@
+name='Sophia'
+print(name)
+age=25
+print(age)
+age=(age+5)
+print(age)
+is_student='True'
+is_student=print(age>18)#как пример,лог.операция,если человеку больше 18 лет, то он может являться студентом.
